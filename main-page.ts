2e88fb9bd9cbe15fafc297ab8999857bd25a0a46
samfreitasxs <samuel.sfdf@gmail.com>
2024-02-24 16:45:54 -0300

    first commit

--- main-page.ts
+++ main-page.ts
@@ -14,3 +14,20 @@ async logout(){
     await this.page.click('a[href="/logout"]');
   }
 }
+
+import type { Page } from '@playwright/test';
+import { expect } from '@playwright/test';
+
+export class MainPage {
+    readonly page: Page;
+constructor(page: Page) {
+        this.page = page;
+    }
+async userIsLogged(){
+    return await expect(this.page.getByText('You logged into a secure area!')).toBeVisible();
+  }
+
+async logout(){
+    await this.page.click('a[href="/logout"]');
+  }
+}
